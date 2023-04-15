041176ba9515611b51b9c7f814eb5b5a1f990cb6
Qiu168 <1462909477@qq.com>
2023-04-15 12:43:43 +0800

    feat(utils):导入js包，自己写了一个common.js

---
+++ project/src/main/webapp/js/common.js
@@ -0,0 +1,11 @@
+let token =sessionStorage.getItem("token");
+axios.interceptors.request.use(
+    config => {
+        if(token) config.headers['authorization'] =token
+        return config
+    },
+    error =>{
+        console.log(error)
+        return Promise.reject(error)
+    }
+)
