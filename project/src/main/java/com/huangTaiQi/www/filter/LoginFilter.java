02c9bcd49d4013ba349c5f7b4da6aa6b5a68ff22
Qiu168 <1462909477@qq.com>
2023-04-16 15:26:38 +0800

    feat(filter):新增检验登录的拦截器

---
+++ project/src/main/java/com/huangTaiQi/www/filter/LoginFilter.java
@@ -0,0 +1,32 @@
+package com.huangTaiQi.www.filter;
+
+
+import com.huangTaiQi.www.utils.UserHolder;
+
+
+import javax.servlet.FilterChain;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * @author 14629
+ */
+
+public class LoginFilter extends BaseFilter{
+    @Override
+    protected boolean doBeforeProcessing(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws Exception {
+        //TODO:放行不用注册的页面
+        //判断是否需要拦截（ThreadLocal中是否有用户）
+        if (UserHolder.getUser() == null) {
+            // 没有，需要拦截，设置状态码
+            response.setStatus(401);
+            // 拦截
+            response.sendRedirect("http://localhost:8080/project_war/html/error/404.html");
+            return false;
+        }
+        // 有用户，则放行
+        return true;
+    }
+}
