e6314674d4bc6b3bee92dced52fe250955e38fa6
Qiu168 <1462909477@qq.com>
2023-04-16 15:19:50 +0800

    feat(utils):RandomUtils和StringUtils

---
+++ project/src/main/java/com/huangTaiQi/www/utils/RandomUtils.java
@@ -0,0 +1,39 @@
+package com.huangTaiQi.www.utils;
+
+
+import java.util.Random;
+
+/**
+ * @author 14629
+ */
+public class RandomUtils {
+
+    private static final String ALL_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    private static final Random random = new Random();
+
+    /**
+     * 生成一个指定范围内的随机整数
+     * @param min 最小值
+     * @param max 最大值
+     * @return 生成的随机整数
+     */
+    public static int getRandomInt(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    /**
+     * 生成一个指定长度的随机字符串
+     * @param length 字符串长度
+     * @return 生成的随机字符串
+     */
+    public static String getRandomString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(ALL_CHARS.length());
+            sb.append(ALL_CHARS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
+}
+
