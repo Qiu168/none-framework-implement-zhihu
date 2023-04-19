5514d60e00be068e912c5676d1e1315faba56db0
Qiu168 <1462909477@qq.com>
2023-04-19 12:15:36 +0800

    feat(entity):新增categoryEntity

---
+++ project/src/main/java/com/huangTaiQi/www/model/entity/CategoryEntity.java
@@ -0,0 +1,26 @@
+package com.huangTaiQi.www.model.entity;
+
+/**
+ * @author 14629
+ */
+@SuppressWarnings("ALL")
+public class CategoryEntity {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
