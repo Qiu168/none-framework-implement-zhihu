c63e753f01f64887d832f07a3a816848fcb2b92f
Qiu168 <1462909477@qq.com>
2023-04-19 09:55:04 +0800

    feat(Transaction):新增事务代理

---
+++ my-framework/src/main/java/com/my_framework/www/Transaction/TransactionHandler.java
@@ -0,0 +1,39 @@
+package com.my_framework.www.Transaction;
+
+import com.my_framework.www.pool.DataBaseUtil;
+import com.my_framework.www.redis.JedisUtils;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author 14629
+ */
+public class TransactionHandler implements MethodInterceptor {
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        Object result = null;
+        try {
+            // 开启事务
+            DataBaseUtil.beginTransaction();
+            JedisUtils.beginTransaction();
+            result = proxy.invokeSuper(obj, args);
+            // 提交事务
+            DataBaseUtil.commitTransaction();
+            JedisUtils.commitTransaction();
+        } catch (Exception e) {
+            // 回滚事务
+            DataBaseUtil.rollbackTransaction();
+            JedisUtils.rollbackTransaction();
+            e.printStackTrace();
+        } finally {
+            // 关闭事务
+            DataBaseUtil.close();
+            JedisUtils.close();
+        }
+        return result;
+    }
+
+}
