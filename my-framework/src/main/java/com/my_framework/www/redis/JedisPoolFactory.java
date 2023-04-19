745eb9e54d56545920af398c88289fee4b891b60
Qiu168 <1462909477@qq.com>
2023-04-19 09:57:43 +0800

    feat(redis):新增管理redis

---
+++ my-framework/src/main/java/com/my_framework/www/redis/JedisPoolFactory.java
@@ -0,0 +1,34 @@
+package com.my_framework.www.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * TODO:事务
+ * @author 14629
+ */
+public class JedisPoolFactory {
+    private static final JedisPool JEDIS_POOL;
+    private static final Logger logger= Logger.getLogger(JedisPoolFactory.class.getName());
+
+    static {
+        JedisPoolConfig jedisPoolConfig=new JedisPoolConfig();
+        //最大连接数
+        jedisPoolConfig.setMaxTotal(16);
+        //最大空闲连接
+        jedisPoolConfig.setMaxIdle(16);
+        //最小空闲连接
+        jedisPoolConfig.setMinIdle(0);
+        //最大等待时间 200ms
+        jedisPoolConfig.setMaxWait(Duration.ofMillis(500));
+        JEDIS_POOL=new JedisPool(jedisPoolConfig,"192.168.181.132",6379,1000,"redis");
+    }
+    public static Jedis getResource(){
+        return JEDIS_POOL.getResource();
+    }
+}
---
+++ my-framework/src/main/java/com/my_framework/www/redis/JedisUtils.java
@@ -0,0 +1,49 @@
+package com.my_framework.www.redis;
+
+import com.my_framework.www.redis.JedisPoolFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author 14629
+ */
+public class JedisUtils {
+    private static final ThreadLocal<Jedis> TL = new ThreadLocal<>();
+    private static final ThreadLocal<Transaction> TRAN = new ThreadLocal<>();
+    private static final Logger logger= Logger.getLogger(JedisUtils.class.getName());
+    public static Jedis getJedis()  {
+        logger.log(Level.INFO,"获取jedis对象");
+        Jedis jedis = TL.get();
+        if (jedis == null) {
+            jedis = JedisPoolFactory.getResource();
+            TL.set(jedis);
+        }
+        return jedis;
+    }
+    public static void close() throws SQLException {
+        Jedis jedis = getJedis();
+        if (jedis != null) {
+            jedis.close();
+            TL.remove();
+        }
+        logger.log(Level.INFO,"归还jedis对象");
+    }
+    public static void beginTransaction(){
+        Jedis jedis = getJedis();
+        Transaction multi = jedis.multi();
+        TRAN.set(multi);
+    }
+    public static void commitTransaction() throws SQLException {
+        TRAN.get().exec();
+        TRAN.remove();
+    }
+
+    public static void rollbackTransaction() throws SQLException {
+        TRAN.get().discard();
+        TRAN.remove();
+    }
+}
