76c8cc27ac8a17cc1f698ccb4ee67caa09a254ad
Qiu168 <1462909477@qq.com>
2023-04-24 23:05:10 +0800

    feat(transaction):暂时关闭redis事务

--- my-framework/src/main/java/com/my_framework/www/transaction/TransactionHandler.java
+++ my-framework/src/main/java/com/my_framework/www/transaction/TransactionHandler.java
@@ -8,6 +8,7 @@ import net.sf.cglib.proxy.MethodProxy;
 import java.lang.reflect.Method;
 
 /**
+ * 问题：有必要redis事务吗？
  * @author 14629
  */
 public class TransactionHandler implements MethodInterceptor {
@@ -18,20 +19,20 @@ public class TransactionHandler implements MethodInterceptor {
         try {
             // 开启事务
             DataBaseUtil.beginTransaction();
-            JedisUtils.beginTransaction();
+            //JedisUtils.beginTransaction();
             result = proxy.invokeSuper(obj, args);
             // 提交事务
             DataBaseUtil.commitTransaction();
-            JedisUtils.commitTransaction();
+            //JedisUtils.commitTransaction();
         } catch (Exception e) {
             // 回滚事务
             DataBaseUtil.rollbackTransaction();
-            JedisUtils.rollbackTransaction();
+            //JedisUtils.rollbackTransaction();
             e.printStackTrace();
         } finally {
             // 关闭事务
             DataBaseUtil.close();
-            JedisUtils.close();
+            //JedisUtils.close();
         }
         return result;
     }
