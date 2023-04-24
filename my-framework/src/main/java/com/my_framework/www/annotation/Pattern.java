50706080f373280a8ff51a52a644431b49fb9897
Qiu168 <1462909477@qq.com>
2023-04-24 23:02:25 +0800

    feat(anno):完善注解

--- my-framework/src/main/java/com/my_framework/www/annotation/Pattern.java
+++ my-framework/src/main/java/com/my_framework/www/annotation/Pattern.java
@@ -1,8 +1,13 @@
 package com.my_framework.www.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * @author 14629
  */
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Pattern {
     /**
      * 默认为非空
