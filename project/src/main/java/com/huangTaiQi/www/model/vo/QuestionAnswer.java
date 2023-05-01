c113ef286808d5bc46692cf7c63ce0f42040239d
Qiu168 <1462909477@qq.com>
2023-05-01 15:49:20 +0800

    feat(VO):新增两个传给前端的类

---
+++ project/src/main/java/com/huangTaiQi/www/model/vo/QuestionAnswer.java
@@ -0,0 +1,70 @@
+package com.huangTaiQi.www.model.vo;
+
+/**
+ * @author 14629
+ */
+public class QuestionAnswer {
+    private Long id;
+    private Long questionId;
+    private Integer state;
+    private String title;
+    private String content;
+    private Long likes;
+    private String questionTitle;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Long likes) {
+        this.likes = likes;
+    }
+
+    public String getQuestionTitle() {
+        return questionTitle;
+    }
+
+    public void setQuestionTitle(String questionTitle) {
+        this.questionTitle = questionTitle;
+    }
+}
