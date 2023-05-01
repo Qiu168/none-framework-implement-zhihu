0a7668331850fa076528f0843293ff5ca2a807ff
Qiu168 <1462909477@qq.com>
2023-05-01 15:51:44 +0800

    feat(controller):新增AnswerController，CommentController，FollowController

---
+++ project/src/main/java/com/huangTaiQi/www/controller/IFollowController.java
@@ -0,0 +1,4 @@
+package com.huangTaiQi.www.controller;
+
+public interface IFollowController {
+}
---
+++ project/src/main/java/com/huangTaiQi/www/controller/impl/AnswerControllerImpl.java
@@ -0,0 +1,27 @@
+package com.huangTaiQi.www.controller.impl;
+
+import com.huangTaiQi.www.controller.BaseController;
+import com.huangTaiQi.www.controller.IAnswerController;
+import com.huangTaiQi.www.service.impl.AnswerServiceImpl;
+import com.my_framework.www.annotation.Autowired;
+import com.my_framework.www.annotation.Controller;
+import com.my_framework.www.annotation.RequestMapping;
+import com.my_framework.www.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author 14629
+ */
+@Controller
+@RequestMapping("answer")
+public class AnswerControllerImpl extends BaseController implements IAnswerController {
+    @Autowired
+    AnswerServiceImpl answerService;
+    @RequestMapping
+    public void getUserAnswer(@RequestParam("id") String userId, HttpServletResponse response) throws Exception {
+        String json = answerService.getUserAnswer(userId);
+        response.setContentType("text/json;charset=utf-8");
+        response.getWriter().write(json);
+    }
+}
