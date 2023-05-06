b17d25ecabc3175f62faccfc20c5f422a4b64e55
Qiu168 <1462909477@qq.com>
2023-05-06 22:16:50 +0800

    feat(entity):新增blackListEntity

---
+++ project/src/main/java/com/huangTaiQi/www/model/entity/BlackListEntity.java
@@ -0,0 +1,25 @@
+package com.huangTaiQi.www.model.entity;
+
+/**
+ * @author 14629
+ */
+public class BlackListEntity {
+    private Long uid;
+    private Long bid;
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public Long getBid() {
+        return bid;
+    }
+
+    public void setBid(Long bid) {
+        this.bid = bid;
+    }
+}
