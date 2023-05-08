88afb0c43978a779d9206096b462113d787cb51e
Qiu168 <1462909477@qq.com>
2023-05-08 17:29:17 +0800

    feat(exception):没有权限异常

---
+++ project/src/main/java/com/huangTaiQi/www/exception/AccessDeniedException.java
@@ -0,0 +1,15 @@
+package com.huangTaiQi.www.exception;
+
+/**
+ * @author 14629
+ */
+public class AccessDeniedException extends RuntimeException {
+
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+
+    public AccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
