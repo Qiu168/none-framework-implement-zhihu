1eeeddfd7a78827cc8683affdc7c111974d8f7fa
Qiu168 <1462909477@qq.com>
2023-05-08 17:33:04 +0800

    feat(service):新增权限service

---
+++ project/src/main/java/com/huangTaiQi/www/service/impl/RightServiceImpl.java
@@ -0,0 +1,12 @@
+package com.huangTaiQi.www.service.impl;
+
+import com.huangTaiQi.www.service.RightService;
+
+/**
+ * @author 14629
+ */
+public class RightServiceImpl implements RightService {
+    public void banUserRight(){
+
+    }
+}
