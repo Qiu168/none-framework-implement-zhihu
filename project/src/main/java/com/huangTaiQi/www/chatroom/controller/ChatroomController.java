c36a0fe9c13a0591877d8aee37d2c963506c101f
Qiu168 <1462909477@qq.com>
2023-05-11 15:04:14 +0800

    feat(chatroom.controller):新增邀请互关好友加入群聊

--- project/src/main/java/com/huangTaiQi/www/chatroom/controller/ChatroomController.java
+++ project/src/main/java/com/huangTaiQi/www/chatroom/controller/ChatroomController.java
@@ -1,12 +1,16 @@
 package com.huangTaiQi.www.chatroom.controller;
 
+
 import com.my_framework.www.annotation.RequestMapping;
 import com.my_framework.www.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
+
+/**
+ * @author 14629
+ */
 public interface ChatroomController {
     /**
      * 获取用户加入的群聊
@@ -28,9 +32,9 @@ public interface ChatroomController {
      * 获取群聊中的用户
      * @param roomId 群聊id
      * @param resp resp
-     * @throws IOException 异常
+     * @throws Exception 异常
      */
-    void getUserList(@RequestParam("roomId") String roomId, HttpServletResponse resp) throws IOException;
+    void getUserList(@RequestParam("roomId") String roomId, HttpServletResponse resp) throws Exception;
 
     /**
      * 获取当前的用户
@@ -38,4 +42,13 @@ public interface ChatroomController {
      * @param resp resp
      */
     void getCurrentUser(HttpServletRequest req, HttpServletResponse resp);
+
+    /**
+     * 新增群友
+     * @param roomId 群id
+     * @param username 新增群友的用户名
+     * @param response resp
+     * @throws Exception 异常
+     */
+    void addUser( String roomId, String username, HttpServletResponse response) throws Exception;
 }
