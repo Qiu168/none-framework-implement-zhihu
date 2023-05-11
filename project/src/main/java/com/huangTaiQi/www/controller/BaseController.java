8cc720b34218eb1855f9d3b9107eb4e7ad5e8a81
Qiu168 <1462909477@qq.com>
2023-05-11 15:12:51 +0800

    feat(baseController):变为抽象类，告诉别人就是用来继承的

--- project/src/main/java/com/huangTaiQi/www/controller/BaseController.java
+++ project/src/main/java/com/huangTaiQi/www/controller/BaseController.java
@@ -5,11 +5,10 @@ import com.my_framework.www.annotation.RequestMapping;
 import javax.servlet.http.HttpServlet;
 
 
-
 /**
  * @author 14629
  * 空类,只有继承了BaseController才会被请求转发
  */
 @RequestMapping("api")
-public class BaseController extends HttpServlet {
+public abstract class BaseController extends HttpServlet {
 }
