5bee049e6ff5dde5111ad58172316dafc5eec266
Qiu168 <1462909477@qq.com>
2023-05-11 15:11:53 +0800

    feat(answerController):新增方法

--- project/src/main/java/com/huangTaiQi/www/controller/IAnswerController.java
+++ project/src/main/java/com/huangTaiQi/www/controller/IAnswerController.java
@@ -1,7 +1,72 @@
 package com.huangTaiQi.www.controller;
 
+import com.my_framework.www.annotation.RequestMapping;
+import com.my_framework.www.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  * @author 14629
  */
 public interface IAnswerController {
+    /**
+     * 根据用户获取回答
+     * @param userId userId
+     * @param response resp
+     * @throws Exception 异常
+     */
+    void getUserAnswer(String userId, HttpServletResponse response) throws Exception;
+
+    /**
+     * 根据问题获取回答
+     * @param questionId 问题id
+     * @param response resp
+     * @throws Exception 异常
+     */
+    void getAnswerByQuestionId(String questionId, HttpServletResponse response) throws Exception;
+
+    /**
+     * 根据id获取回答
+     * @param id id
+     * @param response resp
+     * @throws Exception 异常
+     */
+    void getAnswerById(String id, HttpServletResponse response) throws Exception;
+
+    /**
+     * 回答问题
+     * @param title 标题
+     * @param content 内容
+     * @param questionId 回答的问题id
+     * @param response resp
+     * @throws SQLException 异常
+     * @throws IOException 异常
+     */
+    void sendAnswer(String title, String content, String questionId, HttpServletResponse response) throws SQLException, IOException;
+
+    /**
+     * 审核通过回答
+     * @param answerId 回答
+     * @param id uid
+     * @throws Exception 异常
+     */
+    void passAnswer(String answerId, Long id) throws Exception;
+
+    /**
+     * 获取没有审核的问题
+     * @param page 页数
+     * @param size 大小
+     * @param response resp
+     * @throws Exception 异常
+     */
+    void getUncheckedAnswer(int page, int size, HttpServletResponse response) throws Exception;
+
+    /**
+     * 获取没有审核的问题的数量
+     * @param response resp
+     * @throws Exception 异常
+     */
+    void getUncheckedTotal(HttpServletResponse response) throws Exception;
 }
--- project/src/main/java/com/huangTaiQi/www/controller/impl/AnswerControllerImpl.java
+++ project/src/main/java/com/huangTaiQi/www/controller/impl/AnswerControllerImpl.java
@@ -1,14 +1,20 @@
 package com.huangTaiQi.www.controller.impl;
 
+import com.huangTaiQi.www.constant.enums.MessageType;
 import com.huangTaiQi.www.controller.BaseController;
 import com.huangTaiQi.www.controller.IAnswerController;
 import com.huangTaiQi.www.service.impl.AnswerServiceImpl;
+import com.huangTaiQi.www.service.impl.DynamicServiceImpl;
 import com.my_framework.www.annotation.Autowired;
 import com.my_framework.www.annotation.Controller;
 import com.my_framework.www.annotation.RequestMapping;
 import com.my_framework.www.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static com.huangTaiQi.www.constant.StateConstants.MESSAGE_CHECKING;
 
 /**
  * @author 14629
@@ -18,22 +24,61 @@ import javax.servlet.http.HttpServletResponse;
 public class AnswerControllerImpl extends BaseController implements IAnswerController {
     @Autowired
     AnswerServiceImpl answerService;
+    @Autowired
+    DynamicServiceImpl dynamicService;
+    @Override
     @RequestMapping
     public void getUserAnswer(@RequestParam("id") String userId, HttpServletResponse response) throws Exception {
         String json = answerService.getUserAnswer(userId);
         response.setContentType("text/json;charset=utf-8");
         response.getWriter().write(json);
     }
+    @Override
     @RequestMapping
     public void getAnswerByQuestionId(@RequestParam("id") String questionId,HttpServletResponse response) throws Exception {
         String answer = answerService.getAnswerByQuestionId(questionId);
         response.setContentType("text/json;charset=utf-8");
         response.getWriter().write(answer);
     }
+    @Override
     @RequestMapping
     public void getAnswerById(@RequestParam("answerId") String id,HttpServletResponse response) throws Exception {
         String answer = answerService.getAnswerById(id);
         response.setContentType("text/json;charset=utf-8");
         response.getWriter().write(answer);
+    }
+    @Override
+    @RequestMapping(method = "post")
+    public void sendAnswer(@RequestParam("title") String title,
+                           @RequestParam("content") String content,
+                           @RequestParam("questionId") String questionId,
+                           HttpServletResponse response) throws SQLException, IOException {
+        String message = answerService.addAnswer(questionId, title, content);
+        response.setContentType("text/json;charset=utf-8");
+        response.getWriter().write(message);
+    }
+    @Override
+    @RequestMapping("pass")
+    public void passAnswer(@RequestParam("answerId")String answerId,@RequestParam("userId") Long id) throws Exception {
+        //TODO
+        //改变question的state
+        answerService.passAnswer(answerId);
+        //发送动态
+        dynamicService.sendDynamic(MessageType.ANSWER,id,answerId);
+    }
+
+    @Override
+    @RequestMapping
+    public void getUncheckedAnswer(@RequestParam("page") int page,@RequestParam("size") int size, HttpServletResponse response) throws Exception {
+        String answers=answerService.getUncheckedAnswer(page,size);
+        response.setContentType("text/json;charset=utf-8");
+        response.getWriter().write(answers);
+    }
+    @Override
+    @RequestMapping
+    public void getUncheckedTotal(HttpServletResponse response) throws Exception {
+        int count=answerService.getAnswerCountByState(MESSAGE_CHECKING);
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().write(String.valueOf(count));
     }
 }
