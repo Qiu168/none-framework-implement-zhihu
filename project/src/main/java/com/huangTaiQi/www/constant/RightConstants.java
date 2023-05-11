a7b5fd456e47594140d43a3f70d1ccced27d5a78
Qiu168 <1462909477@qq.com>
2023-05-11 15:10:20 +0800

    feat(constant):新增常量

--- project/src/main/java/com/huangTaiQi/www/constant/RightConstants.java
+++ project/src/main/java/com/huangTaiQi/www/constant/RightConstants.java
@@ -4,10 +4,11 @@ package com.huangTaiQi.www.constant;
  * @author 14629
  */
 public class RightConstants {
-    public static final Integer RIGHT_COUNT=5;
+    public static final Integer RIGHT_COUNT=6;
     public static final Integer COMMENT=1;
     public static final Integer ANSWER=2;
     public static final Integer QUESTION=3;
     public static final Integer LIKE=4;
     public static final Integer BAN=5;
+    public static final Integer CHECK=6;
 }
