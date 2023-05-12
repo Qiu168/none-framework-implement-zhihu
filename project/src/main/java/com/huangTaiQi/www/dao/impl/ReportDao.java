e1aa811c565677acb6667d1f342a8a30b1e6f776
Qiu168 <1462909477@qq.com>
2023-05-12 23:06:50 +0800

    feat(dao):新增reportDao

---
+++ project/src/main/java/com/huangTaiQi/www/dao/impl/ReportDao.java
@@ -0,0 +1,29 @@
+package com.huangTaiQi.www.dao.impl;
+
+import com.huangTaiQi.www.dao.BaseDao;
+import com.huangTaiQi.www.dao.IReportDao;
+import com.huangTaiQi.www.utils.sql.SQLBuilder;
+import com.my_framework.www.annotation.Repository;
+import com.my_framework.www.pool.DataBaseUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author 14629
+ */
+@Repository
+public class ReportDao implements IReportDao {
+    private final Connection connection = DataBaseUtil.getConnection();
+    private final BaseDao baseDao=new BaseDao(connection);
+    public void addReport(Long reporterId, String messageId, String type, String content) throws SQLException {
+        String sql=new SQLBuilder("report")
+                .insert("user_id")
+                .insert("message_id")
+                .insert("type")
+                .insert("time")
+                .insert("content")
+                .buildInsert();
+        baseDao.updateCommon(sql,reporterId,messageId,type,System.currentTimeMillis(),content);
+    }
+}
