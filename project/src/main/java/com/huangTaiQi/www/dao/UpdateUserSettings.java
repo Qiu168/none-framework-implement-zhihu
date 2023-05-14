f4a499c46a8fadd7c581b262b2854bd0acb3c398
Qiu168 <1462909477@qq.com>
2023-05-14 14:01:03 +0800

    refactor

--- project/src/main/java/com/huangTaiQi/www/dao/UpdateUserSettings.java
+++ project/src/main/java/com/huangTaiQi/www/dao/UpdateUserSettings.java
@@ -6,5 +6,12 @@ import java.sql.SQLException;
  * @author 14629
  */
 public interface UpdateUserSettings {
+    /**
+     * 跟新冗余字段
+     * @param id id
+     * @param avatar 头像
+     * @param username 用户名
+     * @throws SQLException 异常
+     */
     void updateSettings(Long id, String avatar, String username) throws SQLException;
 }
