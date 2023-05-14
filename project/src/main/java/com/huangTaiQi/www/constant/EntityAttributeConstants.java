e0b8f1c9910e4eb1264ec8d6aa14dd7b585aa966
Qiu168 <1462909477@qq.com>
2023-05-14 17:41:40 +0800

    feat(constants):新增常量类

---
+++ project/src/main/java/com/huangTaiQi/www/constant/EntityAttributeConstants.java
@@ -0,0 +1,10 @@
+package com.huangTaiQi.www.constant;
+
+/**
+ * @author 14629
+ */
+public class EntityAttributeConstants {
+    public static final String ID="id";
+    public static final String STATE="state";
+    public static final String ALL="*";
+}
